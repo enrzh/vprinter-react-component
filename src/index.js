a6622353e13bc4cab19f48a4d4daddeae68bd1a0
enricomini <enricomini@enricominis-Mac-mini.local>
2026-09-11 19:57:31 +0200

    feat: add flexible printer inputs and mobile preview flow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { VirtualPrinter } from './VirtualPrinter.jsx';
 export { cafeReceipt, calculateTotals, formatOrderDate, moneyFormatter } from './receipt.js';
 export { normalizePrinterMarkup, parsePrinterMarkup, printerInputExamples } from './printerMarkup.js';
+export { normalizeTicket, quickTicketExample } from './simpleTicket.js';
